14d06b089f9ba673e5f5215405b5d6b54a94a54b
Robbie Porter <rthomporter@gmail.com>
2020-04-28 16:45:05 -0500

    updated a tags to Link

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { LandingStyles } from "../Styles/formStyle.module.scss";
 
 const Landing = () => {
@@ -6,12 +7,12 @@ const Landing = () => {
     <div className={LandingStyles}>
       <h1>Garbage Man Simulator 5000</h1>
       <div>
-        <a href="/login">
+        <Link to="/login">
           <p>login</p>
-        </a>
-        <a href="/registration">
+        </Link>
+        <Link to="/registration">
           <p>register</p>
-        </a>
+        </Link>
       </div>
     </div>
   );
