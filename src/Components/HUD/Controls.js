8f0d8740081a500d4730e7493718a5e65ea3fc6b
Robbie Porter <rthomporter@gmail.com>
2020-04-28 01:09:11 -0500

    control icons &hover

--- src/Components/HUD/Controls.js
+++ src/Components/HUD/Controls.js
@@ -1,7 +1,18 @@
 import React from "react";
 import {
+  FaArrowCircleUp,
+  FaArrowCircleDown,
+  FaArrowCircleLeft,
+  FaArrowCircleRight,
+  FaStore,
+  FaHandRock,
+  FaHandPaper,
+} from "react-icons/fa";
+import {
   ControlsStyles,
   ControlsDiv,
+  HoverText,
+  text,
 } from "../../Styles/formStyle.module.scss";
 
 const Controls = () => {
@@ -9,13 +20,34 @@ const Controls = () => {
     <div className={ControlsStyles}>
       <div>controls</div>
       <div className={ControlsDiv}>
-        <div>take</div>
-        <div>drop</div>
-        <div>up</div>
-        <div>down</div>
-        <div>left</div>
-        <div>right</div>
-        <div>store</div>
+        <div className={HoverText}>
+          <FaHandRock />
+          <p>take</p>
+        </div>
+        <div className={HoverText}>
+          <FaHandPaper />
+          <p>drop</p>
+        </div>
+        <div className={HoverText}>
+          <FaArrowCircleLeft />
+          <p>move left</p>
+        </div>
+        <div className={HoverText}>
+          <FaArrowCircleUp />
+          <p>move up</p>
+        </div>
+        <div className={HoverText}>
+          <FaArrowCircleRight />
+          <p>move right</p>
+        </div>
+        <div className={HoverText}>
+          <FaArrowCircleDown />
+          <p>move down</p>
+        </div>
+        <div className={HoverText}>
+          <FaStore />
+          <p>store</p>
+        </div>
       </div>
     </div>
   );
